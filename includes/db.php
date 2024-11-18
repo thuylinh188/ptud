29769e9a9c9328dc96a7be64ec2170c78464bac6
thuylinh188 <thuylinh1882004@gmail.com>
2024-11-18 20:10:08 +0700

    init project

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ptud";
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
